bb4ef28c39314934acb8a8e15c806b374a3009b8
Ellon M <mordecaimwema11@gmail.com>
2023-02-06 14:46:20 +0300

    set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<></>}></Route>
+        <Route element={<></>}></Route>
+      </Routes>
     </div>
   )
 }
 
-export default App
+export default App;
