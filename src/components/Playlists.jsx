671981be0c2cd1249a3abcd23d5328493046b488
Ellon M <mordecaimwema11@gmail.com>
2023-02-11 20:37:36 +0300

    implement filter on homepage to filter thge playlists based on track count data

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPlaylists, authenticate } from '../redux/playlists/playlistsSlice';
 import { filterPlaylists } from '../utils/filter';
@@ -9,6 +9,8 @@ function Playlists() {
 
   const accessToken = useSelector((state) => state.playlists?.token);
   const playlistData = useSelector((state) => state?.playlists.playlists[0]?.playlists);
+  const options = ['All playlists', 'Below 70 Tracks', '70 - 99 Tracks', 'Above 100 Tracks'];
+  const [selected, setSelected] = useState(options[0]);
 
   localStorage.setItem('access-token', JSON.stringify(accessToken));
 
@@ -24,18 +26,77 @@ function Playlists() {
 
   const playlists = filterPlaylists(playlistData);
 
+  const [displayedPlaylists, setDisplayedPlaylists] = useState(playlists);
+
+  const display = () => {
+    const modified = [];
+    // default case
+    setDisplayedPlaylists(playlists);
+
+    // below 50 tracks
+    if (selected === options[1]) {
+      playlists.forEach((playlist) => {
+        if (playlist.tracks < 70) {
+          modified.push(playlist);
+          setDisplayedPlaylists(modified);
+        }
+      });
+    }
+    // between 50 and 99
+    if (selected === options[2]) {
+      playlists.forEach((playlist) => {
+        if (playlist.tracks > 70 && playlist.tracks < 99) {
+          modified.push(playlist);
+          setDisplayedPlaylists(modified);
+        }
+      });
+    }
+    if (selected === options[3]) {
+      playlists.forEach((playlist) => {
+        if (playlist.tracks > 100) {
+          modified.push(playlist);
+          setDisplayedPlaylists(modified);
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (selected) {
+      display();
+    }
+  }, [selected]);
+
   return (
-    <ul className="playlists">
-      {playlists?.map((playlist) => (
-        <Playlist
-          key={playlist.id}
-          id={playlist.id}
-          name={playlist.name}
-          image={playlist.image}
-          tracks={playlist.tracks}
-        />
-      ))}
-    </ul>
+    <>
+      <div className="filter-section">
+        <select value={selected} onChange={(e) => setSelected(e.target.value)} className="select-dropdown">
+          {options && options.map((o) => (
+            <option key={o} value={o}>{o}</option>
+          ))}
+        </select>
+      </div>
+      <ul className="playlists">
+        {displayedPlaylists.length < 1 ? playlists.map((playlist) => (
+          <Playlist
+            key={playlist.id}
+            id={playlist.id}
+            name={playlist.name}
+            image={playlist.image}
+            tracks={playlist.tracks}
+          />
+        ))
+          : displayedPlaylists.map((playlist) => (
+            <Playlist
+              key={playlist.id}
+              id={playlist.id}
+              name={playlist.name}
+              image={playlist.image}
+              tracks={playlist.tracks}
+            />
+          ))}
+      </ul>
+    </>
   );
 }
 
